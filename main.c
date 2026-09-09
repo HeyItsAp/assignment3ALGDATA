5915a2c81c5ee5702823308bb38feb6a25b54f7d
HeyitsAp <adrianbalunan63@gmail.com>
2026-09-09 15:15:58 +0200

    Added sum check for two list

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <assert.h>
 
 #define ARRAY_LEN(arr)(sizeof(arr)/sizeof((arr)[0]))
 
@@ -9,8 +10,15 @@ int sum(int list[], int length){
 	}
 	return sum;
 }
+
+int assertSum(int list1[], int list2[], int length1, int length2){
+	int sumList1 = sum(list1, length1);
+	int sumList2 = sum(list2, length2);
+	assert(sumList1 == sumList2);
+	printf("List1 | %d = %d | List2", sumList1, sumList2);
+}
 int main(){
 	int testlist[] = {1,2,3,4};
-	printf("%d\n", sum(testlist, ARRAY_LEN(testlist)));
-	
+	int testlist2[] = {1,2,3,4};
+	assertSum(testlist, testlist2, ARRAY_LEN(testlist), ARRAY_LEN(testlist2));
 }
