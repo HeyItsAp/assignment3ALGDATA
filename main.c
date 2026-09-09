ccad6c3234e878327b6607f5f61cc0a2366d936f
HeyitsAp <adrianbalunan63@gmail.com>
2026-09-09 15:08:33 +0200

    Added helper sum function to figure out sum to assert later

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 # include <stdio.h>
 
+#define ARRAY_LEN(arr)(sizeof(arr)/sizeof((arr)[0]))
+
+int sum(int list[], int length){
+	int sum = 0;
+	for (int i = 0; i < length; i++){
+		sum += list[i];
+	}
+	return sum;
+}
 int main(){
+	int testlist[] = {1,2,3,4};
+	printf("%d\n", sum(testlist, ARRAY_LEN(testlist)));
 	
 }
